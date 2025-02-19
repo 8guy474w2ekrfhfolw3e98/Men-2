098bfac2e490765bd4b2e7760dc4c493d0cb1707
8guy474w2ekrfhfolw3e98 <8797449@alu.murciaeduca.es>
2025-02-19 13:32:57 +0100

    Add files via upload

--- Men-2-main/Men-2-main/Menú/Menú/player.gd
+++ Men-2-main/Men-2-main/Menú/Menú/player.gd
@@ -1,10 +1,15 @@
 extends Area2D
 
+signal hit
+
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 
-func _ready():
-	screen_size = get_viewport_rect().size
+func _ready(): 
+	
+		hide()
+
+		screen_size = get_viewport_rect().size
 	
 	
 func _process(delta):
@@ -23,3 +28,34 @@ func _process(delta):
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+
+
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+	# See the note below about the following boolean assignment.
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body):
+		pass # Replace with function body.
+	
+
+		hide() # Player disappears after being hit.
+		hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+		$CollisionShape2D.set_deferred("disabled", true)
+
+
+func start(pos):
+		position = pos
+		show()
+		$CollisionShape2D.disabled = false
